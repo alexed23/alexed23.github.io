d244833e57b1b0b29c2fce5f8534109e2f0e3c89
alexed23 <gsnv.alex@gmail.com>
2014-10-11 19:57:13 +0400

    better

--- umbrella/js/initializeSlick.js
+++ umbrella/js/initializeSlick.js
@@ -3,7 +3,33 @@ $(document).ready(function(){
 prevArrow:$('.previous'),
 nextArrow:$('.next'),
 		dots:true,
-		touchMove:false
+		draggable:false,
+		fade:true
 	});
-
+	$('.screenshotCarousel').slick({
+  centerMode: true,
+  centerPadding: '60px',
+  slidesToShow: 5,
+  responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '40px',
+        slidesToShow: 5
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '40px',
+        slidesToShow: 5
+      }
+    }
+  ],
+  touchThreshold:20
+});
 });
---
+++ umbrella/js/slider.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+
+//var index = $('.screenshotCarousel .slick-center').attr("index");
+//alert(index);
+for (var i =1; i >= 3; i++) {
+
+alert('idas');
+
+}
+});
+
+
+
