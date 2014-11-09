0ceb9d037479000c2f236d694025882eed06c50f
alexed23 <gsnv.alex@gmail.com>
2014-11-09 21:12:53 +0300

    upd

--- umbrella/js/initializeSlick.js
+++ umbrella/js/initializeSlick.js
@@ -1,17 +1,29 @@
 $(document).ready(function(){
 	$('.awesomeCarousel').slick({
-prevArrow:$('.previous'),
-nextArrow:$('.next'),
+		prevArrow:$('.previous'),
+		nextArrow:$('.next'),
 		dots:true,
 		draggable:false,
 		fade:true
 	});
 	$('.screenshotCarousel').slick({
-  infinite: true,
-  slidesToShow: 5,
-  slidesToScroll: 3,
-  touchThreshold:20,
-  dots:true
+		infinite: true,
+		slidesToShow: 5,
+		slidesToScroll: 3,
+		touchThreshold:20,
+		dots:true,
+		arrows:false,
+		speed:600
 
-});
+	});
+
+
+	$('.quoteCarousel').slick({
+		infinite: true,
+		dots:true,
+		touchThreshold:35,
+		autoplay:true,
+		speed:500
+	});
+
 });
